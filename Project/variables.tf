25ef29b0f9767d14447d1eb812f166f0489b27ae
praveenrvchndrn <prvn.1996@gmail.com>
2025-02-07 12:23:15 +0530

    Add files via upload

---
+++ Project/variables.tf
@@ -0,0 +1,22 @@
+
+variable "ami_value" {
+    description = "value_for_ami"
+  
+}
+
+variable "instance_type_value" {
+  description = "value_for_instancetype"
+}
+
+variable "aws_bucket" {
+    description = "value_for_s3"
+  
+}
+
+variable "aws_s3_tagname" {
+  
+}
+
+variable "aws_s3_envi" {
+  
+}
